42699983d24441b3a62e74be843b3a38303ecc26
sheepaa <93856335+sheepaa@users.noreply.github.com>
2022-11-26 14:45:34 +0800

    initial

---
+++ front/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import VueAxios from 'vue-axios'
+import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueRouter from 'vue-router';
+import VueWechatTitle from 'vue-wechat-title';
+import router from './router';
+import './assets/css/custom.css';
+import feather from 'feather-icons';
+
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
+Vue.use(VueRouter);
+Vue.use(VueWechatTitle);
+Vue.use(feather);
+Vue.use(VueAxios, axios);
+
+// axios.defaults.baseURL='/api'
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
